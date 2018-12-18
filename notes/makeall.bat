cde2905004f6dec7d4243b38635675757a4d1c49
stephentetley <stephen.tetley@gmail.com>
2018-12-18 12:07:58 +0000

    Adding missed files.

---
+++ notes/makeall.bat
@@ -0,0 +1,15 @@
+@echo on
+
+REM example1
+latex   --output-directory=./out  example1.tex
+dvisvgm --output=./out/example1.svg --bbox=none ./out/example1.dvi
+
+
+REM example2
+latex   --output-directory=./out  example2.tex
+dvipdfm -o ./out/example2.pdf  ./out/example2.dvi
+
+REM example3
+latex   --output-directory=./out  example3.tex
+dvips -o ./out/example3.ps  ./out/example3.dvi
+
